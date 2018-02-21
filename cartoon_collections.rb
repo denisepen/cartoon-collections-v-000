e821429daf746df55c041b28a2a3b10bf8d068ab
Denise Pendleton <dpendl1@hotmail.com>
2018-02-21 23:51:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(array)
   array.any? {|element| element.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  array.include? {|element| element == cheese_types}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
